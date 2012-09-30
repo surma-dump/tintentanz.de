f99c9810e8a40954badcc6216aeb2628bbdf7ab2
Alexander Surma <surma@surmair.de>
2012-09-30 20:04:41 +0200

    Change album behaviour

--- js/album.js
+++ js/album.js
@@ -1,7 +1,7 @@
 define(['jquery'], function($) {
 	var $pages = $('#album img');
-	var num_pages = $pages.length;
-	$($pages.get(0)).toggleClass('active');
+	var num_pages = $pages.length-1;
+	$($pages.get(num_pages)).toggleClass('active');
 	$pages.each(function(id) {
 		$($pages.get(id)).click(function() {
 			$($pages.get(id)).toggleClass('active');
